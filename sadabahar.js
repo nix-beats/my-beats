5ef3b171de9c0e2002b28016c5fb2a9588767204
nix-beats <rahul292864@gmail.com>
2026-08-27 22:18:13 +0530

    Create sadabahar.js

---
+++ sadabahar.js
@@ -0,0 +1,5 @@
+const sadabaharSongs = [
+    { id: "vS3_72h9-jg", title: "Tere Bina Zindagi Se", artist: "Lata Mangeshkar" },
+    { id: "JvU_n9aU1-Q", title: "Pal Pal Dil Ke Paas", artist: "Kishore Kumar" },
+    { id: "t0H1pE-9W7k", title: "Lag Jaa Gale", artist: "Lata Mangeshkar" }
+];
