9191820acc5fc5fdad7a69aca9d669bbceceda38
nix-beats <rahul292864@gmail.com>
2026-08-27 22:46:42 +0530

    Update retro.js

--- retro.js
+++ retro.js
@@ -1,4 +1,3 @@
-const retroSongs = [
-    { id: "1m-H7-1Vn1A", title: "Tamma Tamma Loge", artist: "Bappi Lahiri" },
-    { id: "3Qx_2S2Q4xE", title: "Dil Deewana", artist: "S. P. Balasubrahmanyam" }
+window.retroSongs = [
+    { id: "1m-H7-1Vn1A", title: "Tamma Tamma Loge", artist: "Bappi Lahiri" }
 ];
