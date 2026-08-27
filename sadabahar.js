c412d30e72812cf1e9b879dfb4209ce210d3bfca
nix-beats <rahul292864@gmail.com>
2026-08-27 23:52:36 +0530

    Update sadabahar.js

--- sadabahar.js
+++ sadabahar.js
@@ -1,8 +1,8 @@
 window.sadhabharSongs = [
-    { id: "vS3_72h9-jg", title: "Tere Bina Zindagi Se", artist: "Lata & Kishore" },
-    { id: "85y1xP8bJ50", title: "Lag Ja Gale", artist: "Lata Mangeshkar" },
+    { id: "NwDFa5kKe5g", title: "Tere Bina Zindagi Se", artist: "Lata & Kishore" },
+    { id: "P4ofSL-n3s0", title: "Lag Ja Gale", artist: "Lata Mangeshkar" },
     { id: "1V_394V8E1c", title: "Pal Pal Dil Ke Paas", artist: "Kishore Kumar" },
-    { id: "h0L8iB8b-08", title: "Ek Ajnabee Haseena Se", artist: "Kishore Kumar" }
+    { id: "vS_JXshiu68", title: "Ek Ajnabee Haseena Se", artist: "Kishore Kumar" }
 ];
 
 // बैकअप सुरक्षित रखने के लिए
