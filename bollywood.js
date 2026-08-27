a4d9f7e8ad1a8e3aabf681be72472c5e15096877
nix-beats <rahul292864@gmail.com>
2026-08-27 22:28:05 +0530

    Update bollywood.js

--- bollywood.js
+++ bollywood.js
@@ -1,5 +1,5 @@
 const bollywoodSongs = [
-    { id: "BddP6PYo2gs", title: "Kesariya", artist: "Arijit Singh" },
-    { id: "VAdGW7QDJIs", title: "Chaleya", artist: "Anirudh Ravichander" },
-    { id: "4K7N9Z5U-10", title: "Apna Bana Le", artist: "Arijit Singh" }
+    { id: "6mr4cYJ7yew", title: "Kesariya", artist: "Arijit Singh" },
+    { id: "VAdGW7QDJiU", title: "Chaleya", artist: "Anirudh Ravichander" },
+    { id: "UEvOsQBu1jY", title: "Apna Bana Le", artist: "Arijit Singh" }
 ];
