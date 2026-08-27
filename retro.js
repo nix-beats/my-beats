2d65979eb7845b655244f87dcedfb3be65ab04ec
nix-beats <rahul292864@gmail.com>
2026-08-27 22:18:46 +0530

    Create retro.js

---
+++ retro.js
@@ -0,0 +1,4 @@
+const retroSongs = [
+    { id: "1m-H7-1Vn1A", title: "Tamma Tamma Loge", artist: "Bappi Lahiri" },
+    { id: "3Qx_2S2Q4xE", title: "Dil Deewana", artist: "S. P. Balasubrahmanyam" }
+];
