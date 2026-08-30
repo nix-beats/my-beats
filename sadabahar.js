97218d2db8efaaffeae4667dc72a8b270973aab5
nix-beats <rahul292864@gmail.com>
2026-08-30 22:29:41 +0530

    Delete sadabahar.js

--- sadabahar.js
+++
@@ -1,6 +0,0 @@
-window.sadabaharSongs = [
-    { id: "vS3_72h9-jg", title: "Tere Bina Zindagi Se", artist: "Lata & Kishore" },
-    { id: "h0L8iB8b-08", title: "Ek Ajnabee Haseena Se", artist: "Kishore Kumar" },
-    { id: "1V_394V8E1c", title: "Pal Pal Dil Ke Paas", artist: "Kishore Kumar" },
-    { id: "85y1xP8bJ50", title: "Lag Ja Gale", artist: "Lata Mangeshkar" }
-];
