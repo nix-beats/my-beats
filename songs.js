8167fc7e0772a76d8de9c9d2a55cc8dcd725c31c
nix-beats <rahul292864@gmail.com>
2026-08-30 23:18:52 +0530

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,28 @@
+// songs.js
+const songsList = [
+    { 
+        title: "Tujhe Dekha Toh", 
+        artist: "Kumar Sanu, Lata Mangeshkar", 
+        id: "36UpkMcvG5I" // Aapki di hui YouTube Video ID
+    },
+    { 
+        title: "Pehla Nasha", 
+        artist: "Udit Narayan, Sadhana Sargam", 
+        id: "_l9mZkQd6g8" 
+    },
+    { 
+        title: "Chura Ke Dil Mera", 
+        artist: "Kumar Sanu, Alka Yagnik", 
+        id: "dZp89fK1N_E" 
+    },
+    { 
+        title: "Mera Dil Bhi Kitna Pagal Hai", 
+        artist: "Kumar Sanu, Alka Yagnik", 
+        id: "8_C1Q12S6yU" 
+    },
+    { 
+        title: "Dheere Dheere Se", 
+        artist: "Kumar Sanu", 
+        id: "nF7yu66gqE4" 
+    }
+];
